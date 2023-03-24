48eab6650706c3c1b9b32631f6b2b79d8a5230f6
Mihir <mihirsharma60@gmail.com>
2023-03-24 23:25:07 +0530

    undefined and null Added

--- Interview_question/1.js
+++ Interview_question/1.js
@@ -5,16 +5,19 @@ const user = [
     id: 1,
     name: "Mihir",
     isActive: true,
+    age:19
   },
   {
     id: 2,
     name: "Abhishek",
     isActive: false,
+    age:22
   },
   {
     id: 3,
     name: "Amit",
     isActive: false,
+    age:16
   },
 ];
 
@@ -54,3 +57,23 @@ const usernames = user.map((user)=>{
 })
 
 console.log(usernames)
+
+//Qusetion => Get only active user from the array of objects
+
+const newuser = user.filter((user)=>user.isActive === true)
+console.log(newuser)
+
+
+//Get name of the active user
+let newusers = user.filter((user)=>user.isActive === true).map((user)=>user.name)
+console.log(newusers)
+
+
+//Qusetion Sort user by its Age
+
+let sorteduser = user.sort((user1,user2)=>user1.age - user2.age)
+console.log(sorteduser)
+
+// print Sorted user Only Name
+let sortedusername = user.sort((user1,user2)=>user1.age - user2.age).map((user)=>user.name)
+console.log(sortedusername)
---
+++ Interview_question/2.js
@@ -0,0 +1,12 @@
+//What will be logged in first example and second exam
+let var1
+console.log(var1)
+console.log(typeof var1)
+
+
+
+let var2 = null
+console.log(var2);
+console.log(typeof var2)
+
+// Undefined means the variable has been declared, but its value has not been assigned. Null means an empty value or a blank value.
