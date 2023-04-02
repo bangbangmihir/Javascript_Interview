b596d1bf6def42f55212014f9d8b0ccecf84afa6
Mihir <mihirsharma60@gmail.com>
2023-04-02 13:23:59 +0530

    Array of objects questions

--- Array_of_object_manipulation/2.js
+++ Array_of_object_manipulation/2.js
@@ -27,24 +27,77 @@ const user = [
   },
 ];
 
-
 function countPropertyOccurrences(arr, prop) {
-    const countObj = {
-       
-      };
+  const countObj = {};
 
-    arr.forEach(obj => {
-      if (countObj[obj[prop]]) {
-        countObj[obj[prop]]++;
-      } else {
-        countObj[obj[prop]] = 1;
-      }
+  arr.forEach((obj) => {
+    if (countObj[obj[prop]]) {
+      countObj[obj[prop]]++;
+    } else {
+      countObj[obj[prop]] = 1;
+    }
+  });
+  return countObj;
+}
+
+console.log(countPropertyOccurrences(user, "employeeName"));
+
+//Return array of objects with occurence
+
+
+const newuser = [
+    {
+      employeeName: "Ram",
+      employeeId: 23,
+    },
+    {
+      employeeName: "Shyam",
+      employeeId: 24,
+    },
+    {
+      employeeName: "Ram",
+      employeeId: 20,
+    },
+    {
+      employeeName: "Ram",
+      employeeId: 20,
+    },
+    {
+      employeeName: "Ram",
+      employeeId: 20,
+    },
+    {
+      employeeName: "Ram",
+      employeeId: 25,
+    },
+    {
+      employeeName: "Kisan",
+      employeeId: 22,
+    },
+    {
+      employeeName: "Shyam",
+      employeeId: 20,
+    },
+  ];
+
+function countPropertyOccurrencesinArray(arr, prop) {
+    const countObj = [];
+  
+    arr.forEach((obj) => {
+
+        const userfind = countObj.find((user)=>user[prop] === obj[prop])
+        if(userfind){
+            userfind["Ocuurence"] += 1
+        }
+        else{
+            countObj.push({
+                [prop]:obj[prop],
+                "Ocuurence":1
+            })
+        }
+ 
     });
     return countObj;
   }
-
-
-  console.log(countPropertyOccurrences(user,"employeeName"))
-
-
-
+  
+  console.log(countPropertyOccurrencesinArray(newuser, "employeeName"));
